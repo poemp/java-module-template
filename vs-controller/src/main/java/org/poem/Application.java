caae52e860db6baca1bcd5e5f1aad6a56e01b1a2
lywworking <xue_2013@sina.com>
2018-12-04 14:48:27 +0800

    静态文件添加配置

--- vs-controller/src/main/java/org/poem/Application.java
+++ vs-controller/src/main/java/org/poem/Application.java
@@ -4,17 +4,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 /**
  * @author poem
  */
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @SpringBootApplication
-public class Application {
+@ServletComponentScan
+public class Application extends SpringBootServletInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
         logger.info("start success");
+    }
+
+    /**
+     *  extends SpringBootServletInitializer
+     * 实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        builder.sources(this.getClass());
+        return super.configure(builder);
     }
 }
